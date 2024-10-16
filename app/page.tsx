a7956361144e7ac6a43a3b12ac0baab2a2b4fda2
DC <dc@dcpos.ch>
2024-10-16 13:30:52 -0700

    daimo pay hello world

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,119 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { useEffect, useState } from "react";
+import { ConnectKitButton } from "@daimo/pay";
+
+type Item = { name: string; description: string; price: number };
+
+const items: Item[] = [
+  { name: "Apples", description: "1lb bag", price: 0.2 },
+  { name: "Oranges", description: "1.5lb bag", price: 0.25 },
+  { name: "Banana", description: "One bunch, organic", price: 0.3 },
+];
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // Daimo Pay can be used in the same way for deposit (to dapp), donate, and invoice flows.
+  // In this example, we demo an e-commerce checkout flow.
+  // Start by letting the user choose what to buy:
+  const [selectedItems, setSelectedItems] = useState<Item[]>([]);
+  const totalUsd = selectedItems.reduce((a, b) => a + b.price, 0); // Dollars
+  const amountUSDC = BigInt(totalUsd * 1e6); // USDC units
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  // Generate a payment ID for the cart:
+  const [payId, setPayId] = useState<string>();
+  useEffect(() => {
+    (async () => {
+      if (selectedItems.length === 0) {
+        setPayId(undefined);
+        return;
+      }
+
+      const payId = await generatePayId(selectedItems, amountUSDC);
+      console.log(
+        `Got payment ID: ${payId} for ${JSON.stringify(selectedItems)}`
+      );
+      setPayId(payId);
+    })();
+  }, [selectedItems, amountUSDC]);
+
+  return (
+    <main>
+      <h1>Daimo Pay Hello World</h1>
+      <div style={{height: '16px'}} />
+      <h2>Select Items</h2>
+      <div style={{height: '8px'}} />
+      <ItemsPicker {...{ selectedItems, setSelectedItems }} />
+      <strong>Total: ${totalUsd.toFixed(2)}</strong>
+      <div style={{height: '8px'}} />
+      {payId != null && <ConnectKitButton payId={payId} />}
+    </main>
+  );
+}
+function ItemsPicker({
+  selectedItems,
+  setSelectedItems,
+}: {
+  selectedItems: Item[];
+  setSelectedItems: (items: Item[]) => void;
+}) {
+  return (
+    <div>
+      {items.map((item: Item) => (
+        <div key={item.name}>
+          <input
+            type="checkbox"
+            id={item.name}
+            checked={selectedItems.some(
+              (selectedItem) => selectedItem.name === item.name
+            )}
+            onChange={(e) => {
+              if (e.target.checked) {
+                setSelectedItems([...selectedItems, item]);
+              } else {
+                setSelectedItems(
+                  selectedItems.filter(
+                    (selectedItem) => selectedItem.name !== item.name
+                  )
+                );
+              }
+            }}
+          />
+          <label htmlFor={item.name}>
+            {item.name} - {item.description} - ${item.price.toFixed(2)}
+          </label>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ))}
     </div>
   );
+}
+
+async function generatePayId(
+  selectedItems: Item[],
+  amountUSDC: bigint
+): Promise<string> {
+  const recipient = {
+    chain: 8453,
+    token: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913", // Base USDC
+    amount: `${amountUSDC}`,
+    address: "0xc60A0A0E8bBc32DAC2E03030989AD6BEe45A874D", // enter your receiving address here
+  };
+
+  const response = await fetch("https://pay.daimo.com/api/generate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Idempotency-Key": "" + Math.random(),
+      "Api-Key": "daimopay-demo",
+    },
+    body: JSON.stringify({
+      intent: "Check out",
+      items: selectedItems,
+      recipient,
+    }),
+  });
+
+  const data = await response.json();
+  console.log(data);
+
+  return data.id;
 }
